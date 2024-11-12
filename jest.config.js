824842bf7f9c8c950054fd76a1a6824ba827d7d5
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-11-12 12:03:49 +0000

    Jest configuration for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+	coveragePathIgnorePatterns: [
+		'index.ts',
+		'<rootDir>/src/connectors/',
+		'<rootDir>/src/classes/',
+		'<rootDir>/src/lib/',
+		'<rootDir>/test/data/',
+	],
 	moduleDirectories: ['node_modules', 'src'], // So Jest can resolve modules
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
