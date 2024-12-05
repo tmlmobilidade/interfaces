2537507f86471d263de86c54224c1639a6de8c40
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-12-05 16:13:06 +0000

    Add validation status enum and refactor Apex T11 and T19 schemas

--- src/interfaces/apex-t11/apex-t11.type.ts
+++ src/interfaces/apex-t11/apex-t11.type.ts
@@ -5,27 +5,117 @@ import { z } from 'zod';
 
 /* * */
 
+export enum ValidationStatus {
+
+	/**
+	 * VALID:
+	 * The card holder had a valid contract for the given context.
+	 */
+	_0_ContractValid = 0,
+
+	/**
+	 * INVALID:
+	 * The card holder already has a valid validation for the given context.
+	 */
+	_1_Antipassback = 1,
+
+	/**
+	 * INVALID:
+	 * The card holder's card is in the black list.
+	 */
+	_2_CardInBlackList = 2,
+
+	/**
+	 * INVALID:
+	 * The validator SAM is in the black list.
+	 */
+	_3_SamInBlackList = 3,
+
+	/**
+	 * VALID:
+	 * The card holder's card is in the white list.
+	 */
+	_4_CardInWhiteList = 4,
+
+	/**
+	 * VALID:
+	 * The card holder's profile is in the white list.
+	 */
+	_5_ProfileInWhiteList = 5,
+
+	/**
+	 * VALID:
+	 * The context allows for validation re-use.
+	 */
+	_6_Interchange = 6,
+
+	/**
+	 * INVALID:
+	 * The validation could not be written to the card.
+	 */
+	_7_Interrupted = 7,
+
+	/**
+	 * INVALID:
+	 * The card holder does not have a valid contract for the given context.
+	 */
+	_8_NoValidContract = 8,
+
+	/**
+	 * INVALID:
+	 * The card holder's card is invalidated.
+	 */
+	_9_CardInvalidated = 9,
+
+	/**
+	 * INVALID:
+	 * The card holder's card or the validator's SAM has no more space for events.
+	 */
+	_10_EventsFull = 10,
+
+	/**
+	 * INVALID:
+	 * The card holder's card does not have enough units for the given context.
+	 */
+	_11_NotEnoughUnits = 11,
+
+	/**
+	 * INVALID:
+	 * The card holder's contract has expired.
+	 */
+	_12_ContractExpired = 12,
+
+	/**
+	 * INVALID:
+	 * The maximum value for the validation status. This is used to validate the status.
+	 */
+	_13_MaxValue = 13,
+}
+
+/* * */
+
 export const ApexT11Schema = DocumentSchema.extend({
 	_raw: z.string(),
 	agency_id: z.string(),
-	apex_version: z.string(),
+	apex_transaction_version: z.string(),
 	card_serial_number: z.string(),
 	device_id: z.string(),
-	extra_trip_id: z.string(),
+	extra_trip_id: z.string().nullable(),
 	line_id: z.string(),
+	mac_ase_counter_value: z.number(),
+	mac_sam_serial_number: z.number(),
 	operational_date: z.string().transform(createOperationalDate).brand('OperationalDate'),
 	pattern_id: z.string(),
 	product_id: z.string(),
 	route_id: z.string(),
-	sam_serial_number: z.number(),
 	stop_id: z.string(),
-	timestamp: z.date(),
 	trip_id: z.string(),
+	validation_status: z.nativeEnum(ValidationStatus),
 	vehicle_id: z.string(),
 }).strict();
 
 /**
- * APEX T11 are APEX trasactions of type 11 that are generated when a card holder touches a validator
+ * APEX T11 are APEX transactions of type 11 that are generated when a card holder touches a validator
  * reader (ex: bus validator, subway gate). These validation transactions represent the card holder's right to travel
  * on a given route, line, or vehicle. T11s have statuses that indicate if the card holder was allowed to travel
  * or not, and with which conditions. A validation also contains information about the card holder's card, the vehicle,
--- src/interfaces/apex-t19/apex-t19.type.ts
+++ src/interfaces/apex-t19/apex-t19.type.ts
@@ -8,27 +8,25 @@ import { z } from 'zod';
 export const ApexT19Schema = DocumentSchema.extend({
 	_raw: z.string(),
 	agency_id: z.string(),
-	apex_version: z.string(),
-	card_serial_number: z.string(),
+	apex_transaction_version: z.string(),
 	device_id: z.string(),
-	extra_trip_id: z.string(),
+	extra_trip_id: z.string().nullable(),
 	line_id: z.string(),
+	mac_ase_counter_value: z.number(),
+	mac_sam_serial_number: z.number(),
 	operational_date: z.string().transform(createOperationalDate).brand('OperationalDate'),
 	pattern_id: z.string(),
-	product_id: z.string(),
 	route_id: z.string(),
-	sam_serial_number: z.number(),
 	stop_id: z.string(),
-	timestamp: z.date(),
 	trip_id: z.string(),
 	vehicle_id: z.string(),
 }).strict();
 
 /**
- * APEX T19 are APEX trasactions of type 19 that are generated everytime the
- * setContext or setT19 functions are called. These functions are used to set
+ * APEX T19 are APEX transactions of type 19 that are generated every time the
+ * setContext or setLocation functions are called. These functions are used to set
  * the service context of the vehicle, allowing for the correct validation of products.
- * In summary, these transactions are generated everytime the vehicle has a change in
- * the current stop ID, route ID, pattern ID, etc.
+ * In summary, these transactions are generated every time the vehicle has a change in
+ * the current stop ID, trip ID, route ID, pattern ID, etc.
  */
 export type ApexT19 = z.infer<typeof ApexT19Schema>;
