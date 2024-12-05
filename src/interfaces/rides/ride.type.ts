8e4f1fc6306dab5865a33cc23e5dd21f824b607b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-12-05 18:02:21 +0000

    Refactor RideSchema to change driver_id and vehicle_id to arrays of strings

--- src/interfaces/rides/ride.type.ts
+++ src/interfaces/rides/ride.type.ts
@@ -26,7 +26,7 @@ export type UpdateRideAnalysisDto = Partial<CreateRideAnalysisDto>;
 export const RideSchema = DocumentSchema.extend({
 	agency_id: z.string(),
 	analysis: z.array(RideAnalysisSchema),
-	driver_id: z.string().nullable(),
+	driver_ids: z.array(z.string()),
 	extension_observed: z.number().nullable(),
 	extension_scheduled: z.number(),
 	hashed_shape_id: z.string(),
@@ -37,19 +37,15 @@ export const RideSchema = DocumentSchema.extend({
 	passengers_estimated: z.number().nullable(),
 	pattern_id: z.string(),
 	plan_id: z.string(),
-	refunds_amount: z.number().nullable(),
-	refunds_count: z.number().nullable(),
 	route_id: z.string(),
 	runtime_observed: z.number().nullable(),
 	runtime_scheduled: z.number(),
-	sales_amount: z.number().nullable().nullable(),
-	sales_count: z.number().nullable().nullable(),
 	start_time_observed: z.date().nullable(),
 	start_time_scheduled: z.date(),
 	status: z.enum(['pending', 'processing', 'complete', 'error']),
 	trip_id: z.string(),
 	validations_count: z.number().nullable(),
-	vehicle_id: z.string().nullable(),
+	vehicle_ids: z.array(z.string()),
 }).strict();
 
 export const CreateRideSchema = RideSchema.partial({ _id: true }).omit({ created_at: true, updated_at: true });
