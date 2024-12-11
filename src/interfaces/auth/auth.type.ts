6e0f696ad40178424823dad9695a7b3ba46589aa
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-12-11 16:02:42 +0000

    Refactor LoginDtoSchema in auth.type.ts to add required_error and message for email and password fields

--- src/interfaces/auth/auth.type.ts
+++ src/interfaces/auth/auth.type.ts
@@ -43,8 +43,14 @@ export type UpdateUserDto = Partial<CreateUserDto>;
 /* * */
 
 export const LoginDtoSchema = z.object({
-	email: z.string(),
-	password: z.string(),
+	email: z.string({
+		required_error: 'Email is required',
+	}).email({
+		message: 'Email must be a valid email address',
+	}),
+	password: z.string({
+		required_error: 'Password is required',
+	}),
 }).strict();
 
 export type LoginDto = { email: Email } & z.infer<typeof LoginDtoSchema>;
