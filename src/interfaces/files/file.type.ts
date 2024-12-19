9f372be75c50aa69319b4935de0c33dc304349c7
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-12-19 13:39:57 +0000

    Refactor: Add created_by and updated_by fields to File interface

--- src/interfaces/files/file.type.ts
+++ src/interfaces/files/file.type.ts
@@ -1,6 +1,11 @@
 export interface File {
 	bucket_path?: string
+	created_at: Date
+	created_by: string
 	metadata?: Record<string, unknown>
 	name: string
+	size: number
 	type: string
+	updated_at: Date
+	updated_by: string
 }
