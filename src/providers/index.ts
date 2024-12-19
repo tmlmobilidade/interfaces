1cbc4f7c7bb66db171b745472cf2025b1bd2f552
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-12-19 18:14:19 +0000

    Refactor: Remove unused storage providers in index.ts

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,6 +2,4 @@ export * from './auth.provider';
 export * from './email.provider';
 
 // Storage
-export * from './storage/aws-storage.provider';
-export * from './storage/cloudflare-storage.provider';
 export * from './storage/storage.interface';
