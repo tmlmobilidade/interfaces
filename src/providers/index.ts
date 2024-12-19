d3f02aa50ed27e80d093298e2ff35f30e70075d9
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-12-19 18:36:09 +0000

    Refactor: Add .env.example file with storage type variable

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,4 +2,6 @@ export * from './auth.provider';
 export * from './email.provider';
 
 // Storage
+export * from './storage/s3-storage.provider';
+export * from './storage/storage.factory';
 export * from './storage/storage.interface';
