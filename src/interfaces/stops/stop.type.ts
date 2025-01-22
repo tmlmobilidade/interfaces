6670de3e6de6a64648180f469b8f9e77d6fa075b
João de Vasconcelos <contact@joao.earth>
2025-01-22 20:00:45 +0000

    Refactor: Enhance StopSchema with additional fields and improved structure

--- src/interfaces/stops/stop.type.ts
+++ src/interfaces/stops/stop.type.ts
@@ -10,13 +10,137 @@ export const StopSchema = DocumentSchema.extend({
 	//
 	// General
 
-	_id: z.string().length(6),
+	_id: z
+		.string()
+		.length(6),
+
+	jurisdiction: z
+		.enum(['ip', 'municipality', 'other', 'unknown'])
+		.default('unknown'),
+
+	name: z
+		.string(),
+
+	operational_status: z
+		.enum(['active', 'inactive', 'provisional', 'seasonal', 'voided'])
+		.default('inactive'),
+
+	short_name: z
+		.string()
+		.optional(),
+
+	tts_name: z
+		.string()
+		.optional(),
+
+	//
+	// Location
+
+	district_id: z
+		.string(),
+
+	latitude: z
+		.number(),
+
+	locality_id: z
+		.string()
+		.optional(),
+
+	longitude: z
+		.number(),
+
+	municipality_id: z
+		.string(),
+
+	parish_id: z
+		.string()
+		.optional(),
+
+	//
+	// Infrastructure
+
 	bench_status: z
 		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
 		.default('unknown'),
-	comments: z
-		.array(CommentSchema)
-		.default([]),
+
+	docking_bay_type: z
+		.enum(['unknown', 'simple_interaction', 'cut_in_road_without_marks', 'cut_in_road_with_marks', 'island', 'peninsula'])
+		.default('unknown'),
+
+	electricity_status: z
+		.enum(['available', 'unavailable', 'unknown'])
+		.default('unknown'),
+
+	flag_status: z
+		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
+		.default('unknown'),
+
+	lighting_status: z
+		.enum(['confortable', 'damaged', 'insuficient', 'moderate', 'unavailable', 'unknown'])
+		.default('unknown'),
+
+	pavement_type: z
+		.enum(['asphalt', 'concrete', 'dirt', 'grass', 'gravel', 'portuguese_stones', 'unknown'])
+		.default('unknown'),
+
+	pole_status: z
+		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
+		.default('unknown'),
+
+	road_type: z
+		.enum(['complementary_itinerary', 'highway', 'main_itinerary', 'national_road', 'regional_road', 'secondary_road', 'unknown'])
+		.default('unknown'),
+
+	shelter_code: z
+		.string()
+		.optional(),
+
+	shelter_maintainer: z
+		.string()
+		.optional(),
+
+	shelter_make: z
+		.string()
+		.optional(),
+
+	shelter_model: z
+		.string()
+		.optional(),
+
+	shelter_status: z
+		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
+		.default('unknown'),
+
+	sidewalk_type: z
+		.enum(['unknown', 'none', 'gutter', 'inaccessible', 'is_ok'])
+		.default('unknown'),
+
+	//
+	// Checks
+
+	last_infrastructure_check: z
+		.coerce
+		.date()
+		.optional(),
+
+	last_infrastructure_maintenance: z
+		.coerce
+		.date()
+		.optional(),
+
+	last_schedules_check: z
+		.coerce
+		.date()
+		.optional(),
+
+	last_schedules_maintenance: z
+		.coerce
+		.date()
+		.optional(),
+
+	//
+	// Facilities
+
 	connections: z
 		.array(z.enum([
 			'ferry',
@@ -30,16 +154,6 @@ export const StopSchema = DocumentSchema.extend({
 			'car_parking',
 		]))
 		.default([]),
-	district_id: z.string(),
-	docking_bay_type: z
-		.enum(['unknown', 'simple_interaction', 'cut_in_road_without_marks', 'cut_in_road_with_marks', 'island', 'peninsula'])
-		.default('unknown'),
-	electricity_status: z
-		.enum(['available', 'unavailable', 'unknown'])
-		.default('unknown'),
-
-	//
-	// Location
 
 	facilities: z
 		.array(z.enum([
@@ -55,67 +169,13 @@ export const StopSchema = DocumentSchema.extend({
 			'pip',
 		]))
 		.default([]),
-	flag_status: z
-		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
-		.default('unknown'),
-	jurisdiction: z
-		.enum(['ip', 'municipality', 'other', 'unknown'])
-		.default('unknown'),
-	last_infrastructure_check: z.coerce.date().optional(),
-	last_infrastructure_maintenance: z.coerce.date().optional(),
-	last_schedules_check: z.coerce.date().optional(),
-	last_schedules_maintenance: z.coerce.date().optional(),
-
-	//
-	// Infrastructure
-
-	latitude: z.number(),
-	lighting_status: z
-		.enum(['confortable', 'damaged', 'insuficient', 'moderate', 'unavailable', 'unknown'])
-		.default('unknown'),
-	locality_id: z.string().optional(),
-	longitude: z.number(),
-	municipality_id: z.string(),
-	name: z.string(),
-	operational_status: z
-		.enum(['active', 'inactive', 'provisional', 'seasonal', 'voided'])
-		.default('inactive'),
-	parish_id: z.string().optional(),
-	pavement_type: z
-		.enum(['asphalt', 'concrete', 'dirt', 'grass', 'gravel', 'portuguese_stones', 'unknown'])
-		.default('unknown'),
-	pole_status: z
-		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
-		.default('unknown'),
-	region_id: z.string(),
-	road_type: z
-		.enum(['complementary_itinerary', 'highway', 'main_itinerary', 'national_road', 'regional_road', 'secondary_road', 'unknown'])
-		.default('unknown'),
-	shelter_code: z.string().optional(),
-
-	//
-	// Checks
-
-	shelter_maintainer: z.string().optional(),
-	shelter_make: z.string().optional(),
-	shelter_model: z.string().optional(),
-	shelter_status: z
-		.enum(['not_applicable', 'unknown', 'is_missing', 'is_damaged', 'is_ok'])
-		.default('unknown'),
 
 	//
 	// Notes & Comments
 
-	short_name: z.string().optional(),
-
-	//
-	// Facilities
-
-	sidewalk_type: z
-		.enum(['unknown', 'none', 'gutter', 'inaccessible', 'is_ok'])
-		.default('unknown'),
-
-	tts_name: z.string().optional(),
+	comments: z
+		.array(CommentSchema)
+		.default([]),
 
 }).strict();
 
