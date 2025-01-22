7eb847b99c01787382f8d290e5fef6abd4e188db
João de Vasconcelos <contact@joao.earth>
2025-01-22 20:00:40 +0000

    Refactor: Update VSCode settings for improved linting and formatting configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import { node } from '@carrismetropolitana/eslint'
 export default [
   ...node,
   {
-    ignorePatterns: ['tsup.config.ts'],
+    // ignorePatterns: ['tsup.config.ts'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-extraneous-class': 'off',
